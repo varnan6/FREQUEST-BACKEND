a5412d4259818b4dbad4ab6b4a391aa0555d97d4
Rajat12826 <pariharrajat078@gmail.com>
2025-10-01 09:44:10 +0530

    Add tsconfig.json for deployment

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import authRoutes from './routes/auth';
 import playersRoutes from './routes/players';
 import leaderboardRoutes from './routes/leaderboard';
-import gameRoutes   from './routes/games';
+// import gameRoutes   from './routes/games';
 
 
 const app = express();
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/players', playersRoutes);
 app.use('/api/leaderboard', leaderboardRoutes);
-app.use('/api/game', gameRoutes);
+// app.use('/api/game', gameRoutes);
 
 
 app.get('/', (req, res) => res.json({ ok: true }));
